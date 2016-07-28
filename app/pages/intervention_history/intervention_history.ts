73959ca45d5e77a8e8b0c628b04b7aea5d4de021
Henri <HenriDepannologue>
2016-07-28 09:41:24 +0200

    first commit

---
+++ app/pages/intervention_history/intervention_history.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/intervention_history/intervention_history.html'
+})
+export class InterventionHistoryPage {
+  constructor(private navCtrl: NavController) {
+  }
+}
